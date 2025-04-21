83946614b922b54085e488df75f27af14157fb3b
Sigvard Dackevall <sigvard@MBPsomtrSigvard.lan>
2025-04-21 17:01:12 +0200

    Late policy added, bug fix

--- examples/mnist-pytorch/test_simulation.py
+++ examples/mnist-pytorch/test_simulation.py
@@ -93,7 +93,7 @@ def send_params_to_kubernetes_pods(helper_tuple):
     
     #print(client.start_session(name=session_name, round_timeout=500, rounds=10))
 
-    print(client.start_session(name=session_name, server_functions=ServerFunctions, round_timeout=600, rounds=50))
+    print(client.start_session(name=session_name, server_functions=ServerFunctions, round_timeout=300, rounds=30))
 
     #client.start_session(name=session_name, server_functions=ServerFunctions)
     #print("Simulation started!")5
